9c94c730d8b7dfc6fbe5ed954a6f3f444c8bd53d
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-03-08 17:39:35 +0530

    update _layout.tsx, app/(tabs)/index.tsx and Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,16 +3,16 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#413c58';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#ffdde2',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#ee2d60',
+    tabIconDefault: '#ee2d60',
     tabIconSelected: tintColorLight,
   },
   dark: {
